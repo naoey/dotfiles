3dc3468871a16a18fafe749603ffbcda4b82400b
naoey <naoey@protonmail.ch>
2025-08-17 22:26:17 +0900

    Add missing Nightfox vim plugin

---
+++ dot_config/nvim/lua/plugins/nightfox.lua
@@ -0,0 +1,13 @@
+-- lua/plugins/nightfox.lua
+return {
+  "EdenEast/nightfox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nightfox").setup({
+      options = {
+        transparent = true,
+      },
+    })
+  end,
+}
