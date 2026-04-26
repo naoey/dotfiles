c44005f8eb339c94770da317952d1ecc579b453c
naoey <naoey@protonmail.ch>
2026-04-26 13:03:44 +0900

    Remove rbenv stuff from PowerShell profile

--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -26,11 +26,3 @@ function ll { ls -la }
 #    Start-Process @args -verb runas
 # }
 
-# rbenv for Windows
-$env:RBENV_ROOT = "C:\Ruby-on-Windows"
-
-# Not easy to download on Github?
-# Use a custom mirror!
-# $env:RBENV_USE_MIRROR = "https://abc.com/abc-<version>"
-
-& "$env:RBENV_ROOT\rbenv\bin\rbenv.ps1" init
